b56edfc553fce8831dd04b15950aeae289bc8e5e
moye12325 <gengkang12325@gmail.com>
2024-05-08 17:53:09 +0800

    add 问题相关界面

--- src/components/MdEdit.vue
+++ src/components/MdEdit.vue
@@ -1,5 +1,10 @@
 <template>
-  <Editor :value="value" :plugins="plugins" @change="handleChange" />
+  <Editor
+    :value="value"
+    :mode="mode"
+    :plugins="plugins"
+    @change="handleChange"
+  />
 </template>
 
 <script setup lang="ts">
@@ -8,6 +13,15 @@ import highlight from "@bytemd/plugin-highlight";
 import { Editor, Viewer } from "@bytemd/vue-next";
 import { ref, withDefaults, defineProps } from "vue";
 
+/**
+ * 定义组件属性类型
+ */
+interface Props {
+  value: string;
+  mode?: string;
+  handleChange: (v: string) => void;
+}
+
 const plugins = [
   gfm(),
   highlight(),
@@ -15,22 +29,19 @@ const plugins = [
 ];
 
 /**
- * 定义组件属性类型
- */
-interface Props {
-  value: string;
-  handleChange: (v: string) => void;
-}
-
-/**
  * 给组件指定初始值
  */
 const props = withDefaults(defineProps<Props>(), {
   value: () => "",
+  mode: () => "split",
   handleChange: (v: string) => {
     console.log(v);
   },
 });
 </script>
 
-<style scoped></style>
+<style>
+.bytemd-toolbar-icon.bytemd-tippy.bytemd-tippy-right:last-child {
+  display: none;
+}
+</style>
